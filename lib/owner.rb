312f7de6ce1a2234a65a0495d2db24e78a7df88a
Richard Kurwitz <kurwitz3@aol.com>
2020-09-01 01:47:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor   :say_species
+attr_reader :name, :species 
+@@all = []
+
+def initialize(name)
+  @name = name
+  @species = "human"
+  @say_species = "I am a human."
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+def self.count 
+  x = self.all.count
+  x
+end 
+
+def self.reset_all
+   self.all.clear
+end 
+
+def cats 
+   new = []
+   Cat.all.collect do |a|
+     if a.owner == self 
+       new << a 
+      end 
+     end
+    new
+end
+
+def dogs 
+  new = [] 
+  Dog.all.collect do |a|
+    if a.owner == self 
+      new << a
+end
+end
+new
+end
+
+def buy_cat(name)
+  Cat.all.each do |z|
+ if  z.name == name
+      cats << z 
+end
+end
+end  
+  
+
+
 end
